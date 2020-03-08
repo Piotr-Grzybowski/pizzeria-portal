2fc42bb76633224f141bd0a2e7a16689e93f7ab9
Piotr Grzybowski <grzybek6@gmail.com>
2020-03-08 20:46:59 +0000

    Create waiter's view template

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,26 +1,13 @@
-import React from 'react';
+import React from "react";
 // import {NavLink, Link} from 'react-router-dom';
-import styles from './Header.module.scss';
-import Icon from '../../common/Icon/Icon';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import PageNav from '../PageNav/PageNav';
+import styles from "./Header.module.scss";
+import PageNav from "../PageNav/PageNav";
 
 class Header extends React.Component {
-  render(){
+  render() {
     return (
       <header className={styles.component}>
-        <Grid>
-          <Row between="md" middle="xs">
-            <Col md={3} lg={2}>
-              <PageNav />
-            </Col>
-            <Col md={3} lg={2}>
-              <div className={styles.contact}>
-                <Icon name='phone' /><span>678.243.8455</span>
-              </div>
-            </Col>
-          </Row>
-        </Grid>
+        <PageNav />
       </header>
     );
   }
--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -1,15 +1,192 @@
 import React from 'react';
-import styles from './Ordering.scss';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
-const Ordering = () => (
-  <div className={styles.component}>
-    <h2>Waiter view</h2>
-    <ul>
-      <li><Link to={`${process.env.PUBLIC_URL}/ordering/new`}>New Order</Link></li>
-      <li><Link to={`${process.env.PUBLIC_URL}/ordering/order/123abc`}>Order with id 123abc</Link></li>
-    </ul>
-  </div>
-);
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-export default Ordering;
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  bar: {
+    backgroundColor: '#222f3e',
+    borderRadius: 1,
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: 3,
+    marginTop: 50,
+    border: '2px solid #222f3e',
+  },
+  addButton: {
+    backgroundColor: '#222f3e',
+    color: '#fff',
+    transition: 'ease 0,5s',
+    "&:hover": {
+      backgroundColor: '#222f3e',
+      opacity: 0.9,
+    }
+  }
+}));
+
+export default function Ordering() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.bar}>
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Table 1" {...a11yProps(0)} />
+          <Tab label="Table 2" {...a11yProps(1)} />
+          <Tab label="Table 3" {...a11yProps(2)} />
+          <Tab label="All orders" {...a11yProps()} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+      <Button
+        variant="contained"
+        className={classes.addButton}
+        startIcon={<Icon>add_circle</Icon>}
+      >
+        Add new Order
+      </Button>
+        <List component="nav" aria-label="orders">
+        <ListItem button>
+          <ListItemText primary="First order from table 1" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 1" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 1" />
+        </ListItem>
+        </List>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      <Button
+        variant="contained"
+        className={classes.addButton}
+        startIcon={<Icon>add_circle</Icon>}
+      >
+        Add new Order
+      </Button>
+      <List component="nav" aria-label="orders">
+        <ListItem button>
+          <ListItemText primary="First order from table 2" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 2" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 2" />
+        </ListItem>
+        </List>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+      <Button
+        variant="contained"
+        className={classes.addButton}
+        startIcon={<Icon>add_circle</Icon>}
+      >
+        Add new Order
+      </Button>
+      <List component="nav" aria-label="orders">
+        <ListItem button>
+          <ListItemText primary="First order from table 3" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 3" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 3" />
+        </ListItem>
+        </List>
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+      <List component="nav" aria-label="orders">
+        <ListItem button>
+          <ListItemText primary="First order from table 1" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 1" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 1" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="First order from table 2" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 2" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 2" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="First order from table 3" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Second order from table 3" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Third order from table 3" />
+        </ListItem>
+        </List>
+      </TabPanel>
+    </div>
+  );
+}
